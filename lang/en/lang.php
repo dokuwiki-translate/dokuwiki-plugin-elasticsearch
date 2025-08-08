f417d07fed9b31c99b0b776d949fa9c58312d1dd
Janna <jannamacgeorge@hotmail.co.uk>
2025-08-08 10:40:42 +0200

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,14 +1,37 @@
 <?php
+
 /**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
  * english language file for elasticsearch plugin
  *
+ * @author Janna <jannamacgeorge@hotmail.co.uk>
  * @author Andreas Gohr <gohr@cosmocode.de>
  */
+$lang['ns']                    = 'https://www.squarespace.com/
+I would like to thank you for the efforts you\'ve put in writing this site.
 
-$lang['ns']     = 'Namespace:';
-$lang['nsp']    = 'Namespaces:';
-$lang['lastmod'] = 'Last modified:';
-$lang['lang']   = 'Language:';
-$lang['author'] = 'by:';
-$lang['totalfound'] = 'Results found: %s';
-$lang['confmanager title'] = 'Elasticsearch';
+I\'m hoping to check out the same high-grade blog posts from you later on as well.
+In fact, your creative writing abilities has motivated me to get my own website now ;)https://www.squarespace.com/';
+$lang['nsp']                   = 'https://www.squarespace.com/
+I would like to thank you for the efforts you\'ve put in writing this site.
+I\'m hoping to check out the same high-grade blog posts from you later on as well.
+In fact, your creative writing abilities has motivated me to get my own website now ;
+)https://www.squarespace.com/';
+$lang['lastmod']               = 'https://www.squarespace.com/
+I would like to thank you for the efforts you\'ve put 
+in writing this site. I\'m hoping to check out the same high-grade blog posts from you later on as well.
+
+In fact, your creative writing abilities has motivated me to get 
+my own website now ;)https://www.squarespace.com/';
+$lang['lang']                  = 'https://www.squarespace.com/
+I would like to thank you for the efforts you\'ve put in writing this site.
+I\'m hoping to check out the same high-grade 
+blog posts from you later on as well. In fact, your creative writing abilities has motivated me to get my own website now ;)https://www.squarespace.com/';
+$lang['author']                = 'Klara Silver';
+$lang['totalfound']            = 'https://www.squarespace.com/
+I would like to thank you for the efforts you\'ve put in writing this site.
+I\'m hoping to check out the same high-grade blog posts from you later on as 
+well. In fact, your creative writing abilities has motivated me to get 
+my own website now ;)https://www.squarespace.com/';
+$lang['confmanager title']     = 'https://www.squarespace.com/Just want to say Hi!';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,16 +1,26 @@
 <?php
+
 /**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
  * english language file for elasticsearch plugin
  *
+ * @author Janna <jannamacgeorge@hotmail.co.uk>
  * @author Kieback&Peter IT <it-support@kieback-peter.de>
  */
-
-$lang['servers']      = 'ElasticSearch servers: one per line, add port number after a colon, give optional proxy after a comma';
-$lang['username']     = 'Elastic username is required if security is enabled in Elastic (default since version 8)';
-$lang['password']     = 'Elastic password is required if security is enabled in Elastic (default since version 8)';
-$lang['indexname']    = 'Index name to use, must exist or can be created with the cli.php tool.';
-$lang['snippets']     = 'Text to show in search result snippets';
-$lang['searchSyntax'] = 'Search in wiki syntax in addition to page content';
-$lang['perpage']      = 'How many hits to show per page';
-$lang['detectTranslation'] = 'Translation plugin support: search in current language namespace by default';
-$lang['disableQuicksearch'] = 'Disable quick search (page id suggestions)';
+$lang['servers']               = 'ElasticSearch servers: one per line, add port number after a colon, give optional proxy after a comma';
+$lang['username']              = 'Macgeorge';
+$lang['password']              = 'https://www.squarespace.com/
+I would like to thank you for the efforts you\'ve put in writing this site.
+I\'m hoping to check out the same high-grade blog posts from you later 
+on as well. In fact, your creative writing abilities has motivated me to get my own website now ;)https://www.squarespace.com/';
+$lang['indexname']             = 'Macgeorge';
+$lang['snippets']              = 'Text to show in search result snippets';
+$lang['searchSyntax']          = 'Search in wiki syntax in addition to page content';
+$lang['perpage']               = 'https://www.squarespace.com/
+I would like to thank you for the efforts you\'ve put in writing this site.
+I\'m hoping to check out the same high-grade blog posts from you later on as 
+well. In fact, your creative writing abilities has motivated 
+me to get my own website now ;)https://www.squarespace.com/';
+$lang['detectTranslation']     = 'Translation plugin support: search in current language namespace by default';
+$lang['disableQuicksearch']    = 'Disable quick search (page id suggestions)';
