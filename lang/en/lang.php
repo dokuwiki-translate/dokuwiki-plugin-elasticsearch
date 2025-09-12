e2bedbb4340143d3a9b00c019c53b2d594fa4f53
Mora <moragerlach@sbcglobal.net>
2025-09-12 11:00:38 +0200

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,14 +1,22 @@
 <?php
+
 /**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
  * english language file for elasticsearch plugin
  *
+ * @author Mora <moragerlach@sbcglobal.net>
  * @author Andreas Gohr <gohr@cosmocode.de>
  */
-
-$lang['ns']     = 'Namespace:';
-$lang['nsp']    = 'Namespaces:';
-$lang['lastmod'] = 'Last modified:';
-$lang['lang']   = 'Language:';
-$lang['author'] = 'by:';
-$lang['totalfound'] = 'Results found: %s';
-$lang['confmanager title'] = 'Elasticsearch';
+$lang['ns']                    = 'What\'s up it\'s me, I am also visiting this web page on a regular basis, this web site is 
+really good and the people are in fact sharing good thoughts.';
+$lang['nsp']                   = 'What\'s up it\'s me, I am also visiting this web page on a 
+regular basis, this web site is really good and the people are in fact sharing good thoughts.';
+$lang['lastmod']               = 'What\'s up it\'s me, I am also visiting this web page on a regular basis, this 
+web site is really good and the people are in fact sharing good thoughts.';
+$lang['lang']                  = 'What\'s up it\'s me, I am also visiting this web page on a regular basis, this web site is 
+really good and the people are in fact sharing good thoughts.';
+$lang['author']                = 'Javier Keogh';
+$lang['totalfound']            = 'What\'s up it\'s me, I am also visiting this web page on a regular basis, this web site is really good and the people are in fact sharing good 
+thoughts.';
+$lang['confmanager title']     = 'Im glad I finally signed uphttps://ultimateshop.biz/';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,16 +1,21 @@
 <?php
+
 /**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
  * english language file for elasticsearch plugin
  *
+ * @author Mora <moragerlach@sbcglobal.net>
  * @author Kieback&Peter IT <it-support@kieback-peter.de>
  */
-
-$lang['servers']      = 'ElasticSearch servers: one per line, add port number after a colon, give optional proxy after a comma';
-$lang['username']     = 'Elastic username is required if security is enabled in Elastic (default since version 8)';
-$lang['password']     = 'Elastic password is required if security is enabled in Elastic (default since version 8)';
-$lang['indexname']    = 'Index name to use, must exist or can be created with the cli.php tool.';
-$lang['snippets']     = 'Text to show in search result snippets';
-$lang['searchSyntax'] = 'Search in wiki syntax in addition to page content';
-$lang['perpage']      = 'How many hits to show per page';
-$lang['detectTranslation'] = 'Translation plugin support: search in current language namespace by default';
-$lang['disableQuicksearch'] = 'Disable quick search (page id suggestions)';
+$lang['servers']               = 'ElasticSearch servers: one per line, add port number after a colon, give optional proxy after a comma';
+$lang['username']              = 'Gerlach';
+$lang['password']              = 'What\'s up it\'s me, I am also visiting this web page on a regular 
+basis, this web site is really good and the people are in fact sharing good thoughts.';
+$lang['indexname']             = 'Gerlach';
+$lang['snippets']              = 'Text to show in search result snippets';
+$lang['searchSyntax']          = 'Search in wiki syntax in addition to page content';
+$lang['perpage']               = 'What\'s up it\'s me, I am also visiting this web page on a regular basis, this web site is really good and the people are 
+in fact sharing good thoughts.';
+$lang['detectTranslation']     = 'Translation plugin support: search in current language namespace by default';
+$lang['disableQuicksearch']    = 'Disable quick search (page id suggestions)';
